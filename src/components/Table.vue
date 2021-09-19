4e3fb0669bde218e1963c8a83c6f89a4301a180d
erolsenol <erol.senol@outlook.com>
2021-09-19 15:52:59 +0300

    commit-2

---
+++ src/components/Table.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <v-data-table  
+        :headers="headers"
+        :items="desserts"
+        item-key="name"
+        class="elevation-1">
+        
+        </v-data-table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            items: []
+        }
+    },
+    methods: {
+        GetData() {
+            const response = await axios.get('https://todowebapi.herokuapp.com/items')
+            console.log(response)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Items.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <table/>
+    </div>
+</template>
+
+<script>
+import Table from '../components/Item.vue'
+export default {
+    components: {
+        Table
+    },
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
